8c37b4260c4312dfc606095662e38f8aae933ec5
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-02 15:38:35 -0700

    modularizing

--- main.js
+++ main.js
@@ -1,67 +1,12 @@
 var express = require('express');
 var multer = require('multer')
 var app = express();
+
+var ocrs = require('./ocrs.js');
+
 app.use(multer({
 	dest: './uploads/'
 }))
-
-var outputPath = 'result.txt';
-
-var appId = 'Split App';
-var password = 'oIGLZOt09upQa/x8O/mHIKU/';
-
-
-var ocrsdkModule = require('./ocrsdk.js');
-var ocrsdk = ocrsdkModule.create(appId, password);
-ocrsdk.serverUrl = "http://cloud.ocrsdk.com";
-
-function downloadCompleted(error) {
-	if (error) {
-		console.log("Error: " + error.message);
-		return;
-	}
-	console.log("Done.");
-}
-
-function processingCompleted(error, taskData) {
-	if (error) {
-		console.log("Error: " + error.message);
-		return;
-	}
-
-	if (taskData.status != 'Completed') {
-		console.log("Error processing the task.");
-		if (taskData.error) {
-			console.log("Message: " + taskData.error);
-		}
-		return;
-	}
-
-	console.log("Processing completed.");
-	console.log("Downloading result to " + outputPath);
-
-	ocrsdk.downloadResult(taskData.resultUrl.toString(), outputPath, downloadCompleted);
-}
-
-function uploadCompleted(error, taskData) {
-	if (error) {
-		console.log("Error: " + error.message);
-		return;
-	}
-
-	console.log("Upload completed.");
-	console.log("Task id = " + taskData.id + ", status is " + taskData.status);
-	if (!ocrsdk.isTaskActive(taskData)) {
-		console.log("Unexpected task status " + taskData.status);
-		return;
-	}
-
-	ocrsdk.waitForCompletion(taskData.id, processingCompleted);
-}
-
-var settings = new ocrsdkModule.ProcessingSettings();
-settings.language = "English";
-settings.exportFormat = "txt";
 
 
 
