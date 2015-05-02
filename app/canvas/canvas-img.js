d3e8205939f95b6ea2d7f2b81e490fdf9331a3b0
Cassidy Williams <cassidoo@gmail.com>
2015-05-02 19:38:13 -0400

    Update rectangle stuff to draw w/ touch

--- app/canvas/canvas-img.js
+++ app/canvas/canvas-img.js
@@ -2,38 +2,57 @@
 
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
-var canvas;
-var con;
-var cx;
-var cy;
-var tap = false; // on tap, initially false
 
 $(function() {
-      canvassetup();
+  drawRect();
 });
 
-function canvassetup() {
-    canvas = document.getElementById('receipt');
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+function drawRect() {
+  var canvas = document.getElementById('receipt');
+  var ctx = canvas.getContext('2d');
+  var rect = { };
+  var drag = false;
+  var touch;
 
-    el.addEventListener("touchstart", handleStart, false);
-    el.addEventListener("touchend", handleEnd, false);
-    el.addEventListener("touchleave", handleEnd, false);
-    el.addEventListener("touchmove", handleMove, false);
-    con = canvas.getContext('2d');
-}
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
 
-function handleStart(e) {
-    cx = e.pageX;
-    cy = e.pageY;
-    tap = true;
-}
+  function init() {
+    canvas.addEventListener("touchstart", handleTouch, false);
+    canvas.addEventListener("touchmove", handleTouch, false);
+    canvas.addEventListener("touchleave", handleEnd, false);
+    canvas.addEventListener("touchend", handleEnd, false);
+  }
 
-function handleMove(e) {
+  function handleTouch(event) {
+    if (event.targetTouches.length === 1) {
+      touch = event.targetTouches[0];
 
-}
+      if (event.type == "touchmove") {
+        if (drag) {
+          rect.w = touch.pageX - rect.startX;
+          rect.h = touch.pageY - rect.startY ;
+          draw();
+        }
+      } else {
+        rect.startX = touch.pageX;
+        rect.startY = touch.pageY;
+        drag = true;
+      }
+    }
+  }
 
-function handleEnd(e) {
-    tap = false;
+  function handleEnd(event) {
+    drag = false;
+  }
+
+  function draw() {
+      ctx.lineWidth="4";
+      ctx.strokeStyle="green";
+    ctx.fillStyle = "rgba(0, 100, 255, 0.2)";
+    ctx.fillRect(rect.startX, rect.startY, rect.w, rect.h);
+    ctx.stroke();
+  }
+
+  init();
 }
