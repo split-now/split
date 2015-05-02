2f0591b64b42dcddcda3e4d1cb050ee5a1c23ee2
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-02 15:16:08 -0700

    adding ocr

--- main.js
+++ main.js
@@ -1,23 +1,97 @@
 var express = require('express');
-var multer  = require('multer')
+var multer = require('multer')
 var app = express();
-app.use(multer({ dest: './uploads/'}))
+app.use(multer({
+	dest: './uploads/'
+}))
 
-var port = process.ENV
+var outputPath = 'result.txt';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var appId = 'Split App';
+var password = 'oIGLZOt09upQa/x8O/mHIKU/';
+
+
+var ocrsdkModule = require('./ocrsdk.js');
+var ocrsdk = ocrsdkModule.create(appId, password);
+ocrsdk.serverUrl = "http://cloud.ocrsdk.com"; 
+
+function downloadCompleted(error) {
+	if (error) {
+		console.log("Error: " + error.message);
+		return;
+	}
+	console.log("Done.");
+}
+
+function processingCompleted(error, taskData) {
+	if (error) {
+		console.log("Error: " + error.message);
+		return;
+	}
+
+	if (taskData.status != 'Completed') {
+		console.log("Error processing the task.");
+		if (taskData.error) {
+			console.log("Message: " + taskData.error);
+		}
+		return;
+	}
+
+	console.log("Processing completed.");
+	console.log("Downloading result to " + outputPath);
+
+	ocrsdk.downloadResult(taskData.resultUrl.toString(), outputPath, downloadCompleted);
+}
+
+function uploadCompleted(error, taskData) {
+	if (error) {
+		console.log("Error: " + error.message);
+		return;
+	}
+
+	console.log("Upload completed.");
+	console.log("Task id = " + taskData.id + ", status is " + taskData.status);
+	if (!ocrsdk.isTaskActive(taskData)) {
+		console.log("Unexpected task status " + taskData.status);
+		return;
+	}
+
+	ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+}
+
+var settings = new ocrsdkModule.ProcessingSettings();
+settings.language = "English"; 
+settings.exportFormat = "txt";
+
+
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
 });
 
 app.post('/api/photos', function(req, res) {
-  console.dir(req.files);
+	console.dir(req.files);
+	res.send('Upload complete');
+
+	var imagePath = '/uploads/image.jpg';
+
+	try {
+		ocrsdk.processImage(imagePath, settings, uploadCompleted);
+
+	} catch (err) {
+		console.log("Error: " + err.message);
+	}
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
+
+
+
