d2310db8c3fd3be644aa1f5e984a14ce14685d89
Justin Woo <justinzw@gmail.com>
2015-05-02 18:33:14 -0700

    add Nexmo

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var multer = require('multer')
 var app = express();
-
 var ocrs = require('./ocrs.js');
+var nexmo = require('easynexmo');
+
+nexmo.initialize('0b3f7f9c','6ea51cfd','http','true');
+
+app.set('view engine', 'ejs');
 
 app.use(multer({
 	dest: './uploads/'
 }))
-
-
 
 app.get('/', function(req, res) {
 	res.send('Hello World!');
@@ -30,7 +32,17 @@ app.post('/api/photos', function(req, res) {
 
 app.get('/showmethemoney', function(req, res){
 	console.log("Hello World");	
-	});
+	res.render('index');
+});
+
+app.get('/nexmo', function(req, res){
+	var tel = req.param('tel');
+	var msg = req.param('msg');
+ 	//nexmo.sendTextMessage('12532715412','14255912367','Helllo I love Nexmo!', '', console.log('Nexmo sent!'));	
+ 	nexmo.sendTextMessage('12532715412', tel, msg, '', console.log('Nexmo sent!'));	
+	console.log("Nexmo sent!!");	
+	res.send('Nexmo');
+});
 
 
 var server = app.listen(3000, function() {
