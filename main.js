ff4ab60a77c1241c159b911ca98ffbda6c864450
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-02 19:03:04 -0700

    switching to tesseract

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ var express = require('express');
 var multer = require('multer')
 var app = express();
 
-var ocrs = require('./ocrs.js');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var tesseract = require('node-tesseract');
+
 
 app.use(multer({
 	dest: './uploads/'
 }))
-
 
 
 app.get('/', function(req, res) {
@@ -18,28 +21,45 @@ app.post('/api/photos', function(req, res) {
 	console.dir(req.files);
 	res.send('Upload complete');
 
-	var imagePath = './uploads/' + req.files.files.name;
+	var imagePath = '/uploads/' + req.files.files.name;
 	console.log(imagePath);
-	try {
-		ocrsdk.processImage(imagePath, settings, uploadCompleted);
 
-	} catch (err) {
-		console.log("Error: " + err.message);
-	}
+	tesseract.process(__dirname + imagePath, function(err, text) {
+		if (err) {
+			console.error(err);
+		} else {
+			var str = text.match(/(\d+\.\d+)\s/g);
+
+			console.log(parseFloat(str[0].substring(0, str[0].length - 1)) + 0.01);
+		}
+	});
 });
 
-app.get('/showmethemoney', function(req, res){
-	console.log("Hello World");	
+app.get('/showmethemoney', function(req, res) {
+	console.log("Hello World");
+});
+// user act as a receiver
+// user act as a master
+
+//flicks, notify all people 
+// charges everyone
+
+io.on('connection', function(socket) {
+
+	socket.on('master', function(data) {
+		socket.emit('master', {
+			masterID: data.venmoID
+		});
+	});
+
+	socket.on('flicked', function(data) { // data  = {venmoID: id}
+		socket.emit('flicked-response', {
+			masterID: data.venmoID
+		});
 	});
 
 
-var server = app.listen(3000, function() {
-
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-
 });
 
 
+server.listen(3000);
